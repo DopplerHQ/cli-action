5579ece45823144eed15256362d122f53cd2fd49
Thomas Piccirello <thomas@doppler.com>
2020-05-21 01:08:22 -0700

    Initial action implementation

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install Doppler CLI action
+
+This action installs the latest [Doppler CLI](https://github.com/DopplerHQ/cli) into your PATH.
+
+## Example usage
+
+```
+uses: dopplerhq/cli-action@v1
+```
+
---
+++ index.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+
+const workspace = process.env.GITHUB_WORKSPACE;
+const binDir = `${workspace}/bin`;
+
+try {
+  run();
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+async function run() {
+  switch (process.platform) {
+    case "win32": {
+      const url = 'https://cli.doppler.com/download?os=windows&arch=amd64&format=zip';
+      await installZip(binDir, url);
+      break;
+    }
+    case "linux": {
+      const url = "https://cli.doppler.com/download?os=linux&arch=amd64&format=tar";
+      await installTar(binDir, url);
+      break;
+    }
+    case "darwin": {
+      const url = "https://cli.doppler.com/download?os=macos&arch=amd64&format=tar";
+      await installTar(binDir, url);
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported platform '${process.platform}'`);
+    }
+  }
+}
+
+async function installTar(path, url) {
+  await io.mkdirP(path);
+  const downloadPath = await tc.downloadTool(url);
+  await tc.extractTar(downloadPath, path);
+  await exec.exec('echo', [`::add-path::${path}`]);
+}
+
+async function installZip(path, url) {
+  await io.mkdirP(path);
+  const downloadPath = await tc.downloadTool(url);
+  await tc.extractZip(downloadPath, path);
+  await exec.exec('echo', [`::add-path::${path}`]);
+}
